5ce657b8d91754587211ded6c0b99c120eb34a50
Zillion <Yoshi@area51hirishima.site>
2023-11-06 03:11:58 +0800

    Win Socket sources

--- JZTunnel-win/Frameworks.h
+++ JZTunnel-win/Frameworks.h
@@ -1,4 +1,7 @@
 #pragma once
 
-#include <Windows.h>
+#include <winsock2.h>
 #include <stdio.h>
+
+#define TUNNEL_IP "input domain name / IP"
+#define TUNNEL_PORT 30000
--- JZTunnel-win/Main.cpp
+++ JZTunnel-win/Main.cpp
@@ -2,42 +2,13 @@
 
 int main() {
 
-	SetConsoleTitleA("Checking Console Size");
-
-	INPUT_RECORD out = { };
-
-	while (out.EventType != KEY_EVENT) {
-		COORD pos = { };
-		pos.X = 0;
-		pos.Y = 0;
-
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-
-		CONSOLE_SCREEN_BUFFER_INFO Consize = { };
-
-		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Consize);
-
-		printf("Size:\nx = %6d\ny = %6d\n\n\n", Consize.dwSize.X, Consize.dwSize.Y);
-
-		printf("Maximum:\nx = %6d\ny = %6d\n\n\n", Consize.dwMaximumWindowSize.X, Consize.dwMaximumWindowSize.Y);
-
-		RECT winconSize = { };
-
-		GetWindowRect(GetConsoleWindow(), &winconSize);
-
-		printf("Window Size:\nx = %6d\ny = %6d\n\n\n\n", winconSize.right - winconSize.left, winconSize.bottom - winconSize.top);
-
-		printf("All space:\nx = %6d\ny = %6d\n", Consize.dwSize.X, Consize.dwCursorPosition.Y);
-
-		Sleep(20);
-
-		DWORD trash;
-
-		if (PeekConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &out, 1, &trash) && trash > 0)
-			ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &out, 1, &trash);
-	}
-
-	SetConsoleTitleA("Size Checked!");
+	/*
+	https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
+	https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-bind
+	https://learn.microsoft.com/en-us/windows/win32/winsock/sockaddr-2
+	https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-sendto
+	https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-recvfrom
+	*/
 
 	return 0;
 }
