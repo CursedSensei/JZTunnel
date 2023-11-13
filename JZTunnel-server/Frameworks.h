7cd054e25ec7a024f856465cf13c43405488eabd
Zillion Linux <yoshi@area51hirishima.site>
2023-11-13 19:58:34 +0800

    Server: Listner and Client sockets

--- JZTunnel-server/Frameworks.h
+++ JZTunnel-server/Frameworks.h
@@ -1,4 +1,18 @@
 #pragma once
 
 #include <stdio.h>
-// find socket header for linux
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SOCKET int
+#define SOCKET_ERROR -1
+
+enum bool {FALSE, TRUE};
+
+#define TUNNEL_PORT 30000
+#define PASS 0xDE4A5F
+
+#include "Socket_Structs.h"
+#include "Utils.h"
+#include "Socket.h"
--- JZTunnel-server/Main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Frameworks.h"
-
-int main() {
-
-
-	return 0;
-}
